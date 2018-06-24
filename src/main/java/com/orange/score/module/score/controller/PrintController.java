63f98bc4c34654e4d20ad4642f82444e4bd7df8e
chenJz1012 <597160667@qq.com>
2018-06-24 17:57:06 +0800

    受理通知书

--- src/main/java/com/orange/score/module/score/controller/PrintController.java
+++ src/main/java/com/orange/score/module/score/controller/PrintController.java
@@ -5,14 +5,12 @@ import com.orange.score.common.core.Result;
 import com.orange.score.common.tools.freemarker.FreeMarkerUtil;
 import com.orange.score.common.utils.ResponseUtil;
 import com.orange.score.common.utils.date.DateUtil;
+import com.orange.score.database.score.model.CompanyInfo;
 import com.orange.score.database.score.model.IdentityInfo;
 import com.orange.score.database.score.model.MaterialInfo;
 import com.orange.score.database.score.model.OnlinePersonMaterial;
 import com.orange.score.database.security.model.Role;
-import com.orange.score.module.score.service.IIdentityInfoService;
-import com.orange.score.module.score.service.IMaterialAcceptRecordService;
-import com.orange.score.module.score.service.IMaterialInfoService;
-import com.orange.score.module.score.service.IOnlinePersonMaterialService;
+import com.orange.score.module.score.service.*;
 import com.orange.score.module.security.SecurityUser;
 import com.orange.score.module.security.SecurityUtil;
 import com.orange.score.module.security.service.RoleService;
@@ -53,6 +51,9 @@ public class PrintController extends BaseController {
     @Autowired
     private IMaterialInfoService iMaterialInfoService;
 
+    @Autowired
+    private ICompanyInfoService iCompanyInfoService;
+
     @GetMapping(value = "/template")
     @ResponseBody
     public Result html() throws FileNotFoundException {
@@ -88,8 +89,8 @@ public class PrintController extends BaseController {
         Date now = new Date();
         String nowYear = String.valueOf(DateUtil.getYear(now));
         params.put("nowYear", nowYear);
-        int newMonth = DateUtil.getMonth(now);
-        String nowMonthStr = String.valueOf(newMonth) + 1;
+        int newMonth = DateUtil.getMonth(now) + 1;
+        String nowMonthStr = String.valueOf(newMonth);
         if (newMonth < 10) {
             nowMonthStr = "0" + nowMonthStr;
         }
@@ -124,6 +125,73 @@ public class PrintController extends BaseController {
 
         String templatePath = ResourceUtils.getFile("classpath:templates/").getPath();
         String html = FreeMarkerUtil.getHtmlStringFromTemplate(templatePath, "accept_doc.ftl", params);
+        Map result = new HashMap<>();
+        result.put("html", html);
+        return ResponseUtil.success(result);
+    }
+
+    @GetMapping(value = "/acceptNotice")
+    @ResponseBody
+    public Result acceptNotice(@RequestParam("personId") Integer personId) throws FileNotFoundException {
+        IdentityInfo identityInfo = iIdentityInfoService.findById(personId);
+        Map params = new HashMap();
+        params.put("person", identityInfo);
+        CompanyInfo companyInfo = iCompanyInfoService.findById(identityInfo.getCompanyId());
+        params.put("company",companyInfo);
+        String year = String.valueOf(DateUtil.getYear(identityInfo.getReservationDate()));
+        params.put("year", year);
+        int month = DateUtil.getMonth(identityInfo.getReservationDate()) + 1;
+        String monthStr = String.valueOf(month);
+        if (month < 10) {
+            monthStr = "0" + monthStr;
+        }
+        params.put("month", monthStr);
+        int day = DateUtil.getDay(identityInfo.getReservationDate());
+        String dayStr = String.valueOf(day);
+        if (day < 10) {
+            dayStr = "0" + dayStr;
+        }
+        params.put("day", dayStr);
+
+        Date now = new Date();
+        String nowYear = String.valueOf(DateUtil.getYear(now));
+        params.put("nowYear", nowYear);
+        int newMonth = DateUtil.getMonth(now) + 1;
+        String nowMonthStr = String.valueOf(newMonth);
+        if (newMonth < 10) {
+            nowMonthStr = "0" + nowMonthStr;
+        }
+        params.put("nowMonth", nowMonthStr);
+        int nowDay = DateUtil.getDay(now);
+        String nowDayStr = String.valueOf(nowDay);
+        if (day < 10) {
+            nowDayStr = "0" + nowDayStr;
+        }
+        params.put("nowDay", nowDayStr);
+        String nowStr = DateUtil.getDate(now);
+        params.put("now", nowStr);
+        SecurityUser user = SecurityUtil.getCurrentSecurityUser();
+        params.put("user", user);
+        List<Integer> roles = userService.findUserRoleByUserId(user.getId());
+        Role role = roleService.findRoleById(roles.get(0));
+        params.put("department", role.getRoleName());
+        List<MaterialInfo> materialInfos = iMaterialInfoService.findAll();
+        Map mMap = new HashMap();
+        for (MaterialInfo materialInfo : materialInfos) {
+            mMap.put(materialInfo.getId() + "", materialInfo.getName());
+        }
+        Condition condition = new Condition(OnlinePersonMaterial.class);
+        tk.mybatis.mapper.entity.Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("personId", identityInfo.getId());
+        criteria.andEqualTo("batchId", identityInfo.getBatchId());
+        List<OnlinePersonMaterial> onlinePersonMaterials = iOnlinePersonMaterialService.findByCondition(condition);
+        for (OnlinePersonMaterial onlinePersonMaterial : onlinePersonMaterials) {
+            onlinePersonMaterial.setMaterialInfoName((String) mMap.get(onlinePersonMaterial.getMaterialInfoId() + ""));
+        }
+        params.put("mList", onlinePersonMaterials);
+
+        String templatePath = ResourceUtils.getFile("classpath:templates/").getPath();
+        String html = FreeMarkerUtil.getHtmlStringFromTemplate(templatePath, "accept_notice_doc.ftl", params);
         Map result = new HashMap<>();
         result.put("html", html);
         return ResponseUtil.success(result);
