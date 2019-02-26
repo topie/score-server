1423005f98499d4bd9927e54edc893f7de039392
xueguangrui <10504228262qq.com>
2019-02-26 09:42:30 +0800

    预审排序显示有问题

--- src/main/java/com/orange/score/module/score/controller/approve/RenshePrevApproveController.java
+++ src/main/java/com/orange/score/module/score/controller/approve/RenshePrevApproveController.java
@@ -99,8 +99,10 @@ public class RenshePrevApproveController {
             identityInfo.setAcceptAddressId(2);
         }
         identityInfo.setUnionApproveStatus2(1);
-        identityInfo.setOrderByColumn("unionApprove2Et");
-        identityInfo.setOrderBy("asc");
+        identityInfo.setOrderByColumn("reservationDate");
+        identityInfo.setOrderBy("desc");
+//        identityInfo.setOrderByColumn("unionApprove2Et");
+//        identityInfo.setOrderBy("asc");
         PageInfo<IdentityInfo> pageInfo = iIdentityInfoService.selectByFilterAndPage(identityInfo, pageNum, pageSize);
 
         List<Integer> companyIds = iIdentityInfoService.selectApprovingRedCompanyId(identityInfo, 5);
