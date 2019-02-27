569d4b335b5ef7597f062cdcd633f4d30640387b
xueguangrui <10504228262qq.com>
2019-02-27 14:43:01 +0800

    虚假材料库的留痕记录

--- src/main/java/com/orange/score/module/score/controller/FakeRecordController.java
+++ src/main/java/com/orange/score/module/score/controller/FakeRecordController.java
@@ -2,15 +2,25 @@ package com.orange.score.module.score.controller;
 
 import com.github.pagehelper.PageInfo;
 import com.orange.score.common.core.Result;
+import com.orange.score.common.exception.AuthBusinessException;
 import com.orange.score.common.tools.plugins.FormItem;
 import com.orange.score.common.utils.PageConvertUtil;
 import com.orange.score.common.utils.ResponseUtil;
+import com.orange.score.database.score.model.BatchConf;
 import com.orange.score.database.score.model.FakeRecord;
+import com.orange.score.database.score.model.IdentityInfo;
+import com.orange.score.database.score.model.PersonBatchStatusRecord;
 import com.orange.score.module.core.service.ICommonQueryService;
 import com.orange.score.module.score.service.IFakeRecordService;
+import com.orange.score.module.score.service.IIdentityInfoService;
+import com.orange.score.module.score.service.IPersonBatchStatusRecordService;
+import com.orange.score.module.security.SecurityUser;
+import com.orange.score.module.security.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tk.mybatis.mapper.entity.Condition;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,6 +35,12 @@ public class FakeRecordController {
 
     @Autowired
     private ICommonQueryService iCommonQueryService;
+
+    @Autowired
+    private IPersonBatchStatusRecordService iPersonBatchStatusRecordService;
+
+    @Autowired
+    private IIdentityInfoService iIdentityInfoService;
 
     @GetMapping(value = "/list")
     @ResponseBody
@@ -45,18 +61,87 @@ public class FakeRecordController {
     @PostMapping("/insert")
     public Result insert(FakeRecord fakeRecord) {
         iFakeRecordService.save(fakeRecord);
-        return ResponseUtil.success();
+        /*
+        2019年2月27日，添加在虚假材料库中添加此人的留痕记录
+         */
+        Condition condition = new Condition(IdentityInfo.class);
+        tk.mybatis.mapper.entity.Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("idNumber", fakeRecord.getIdNumber());
+        criteria.andEqualTo("batchId", fakeRecord.getBatchCode());
+        List<IdentityInfo> list = iIdentityInfoService.findByCondition(condition);
+        if (list.size() == 0){
+            return ResponseUtil.error("请输入正确的身份证号，批次信息");
+        } else {
+
+            SecurityUser securityUser = SecurityUtil.getCurrentSecurityUser();
+            if (securityUser == null) throw new AuthBusinessException("用户未登录");
+
+            PersonBatchStatusRecord personBatchStatusRecord = new PersonBatchStatusRecord();
+            personBatchStatusRecord.setPersonId(list.get(0).getId());
+            personBatchStatusRecord.setBatchId(list.get(0).getBatchId());
+            personBatchStatusRecord.setPersonIdNumber(list.get(0).getIdNumber());
+            personBatchStatusRecord.setStatusStr(securityUser.getLoginName()+"-录入虚假材料信息");
+            personBatchStatusRecord.setStatusTime(new Date());
+            personBatchStatusRecord.setStatusReason("申请人提供虚假信息办理积分落户");
+            personBatchStatusRecord.setStatusTypeDesc("5年内有提供虚假信息申办积分入户记录的（包括虚假户籍、学历、专业、就业、住房、投资、纳税证明等），每条减30分");
+            personBatchStatusRecord.setStatusInt(101);
+            iPersonBatchStatusRecordService.save(personBatchStatusRecord);
+            return ResponseUtil.success();
+        }
     }
 
     @PostMapping("/delete")
     public Result delete(@RequestParam Integer id) {
+
+        FakeRecord fakeRecord = iFakeRecordService.findById(id);
+        Condition condition = new Condition(IdentityInfo.class);
+        tk.mybatis.mapper.entity.Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("idNumber", fakeRecord.getIdNumber());
+        criteria.andEqualTo("batchId", fakeRecord.getBatchCode());
+        List<IdentityInfo> list = iIdentityInfoService.findByCondition(condition);
         iFakeRecordService.deleteById(id);
+        SecurityUser securityUser = SecurityUtil.getCurrentSecurityUser();
+        if (securityUser == null) throw new AuthBusinessException("用户未登录");
+
+        PersonBatchStatusRecord personBatchStatusRecord = new PersonBatchStatusRecord();
+        personBatchStatusRecord.setPersonId(list.get(0).getId());
+        personBatchStatusRecord.setBatchId(list.get(0).getBatchId());
+        personBatchStatusRecord.setPersonIdNumber(list.get(0).getIdNumber());
+        personBatchStatusRecord.setStatusStr(securityUser.getLoginName()+"-删除虚假材料信息");
+        personBatchStatusRecord.setStatusTime(new Date());
+        personBatchStatusRecord.setStatusReason("删除虚假材料信息");
+        personBatchStatusRecord.setStatusTypeDesc("删除虚假材料信息");
+        personBatchStatusRecord.setStatusInt(102);
+        iPersonBatchStatusRecordService.save(personBatchStatusRecord);
+
         return ResponseUtil.success();
     }
 
     @PostMapping("/update")
     public Result update(FakeRecord fakeRecord) {
         iFakeRecordService.update(fakeRecord);
+
+        FakeRecord fakeRecord2 = iFakeRecordService.findById(fakeRecord.getId());
+        Condition condition = new Condition(IdentityInfo.class);
+        tk.mybatis.mapper.entity.Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("idNumber", fakeRecord2.getIdNumber());
+        criteria.andEqualTo("batchId", fakeRecord2.getBatchCode());
+        List<IdentityInfo> list = iIdentityInfoService.findByCondition(condition);
+
+        SecurityUser securityUser = SecurityUtil.getCurrentSecurityUser();
+        if (securityUser == null) throw new AuthBusinessException("用户未登录");
+
+        PersonBatchStatusRecord personBatchStatusRecord = new PersonBatchStatusRecord();
+        personBatchStatusRecord.setPersonId(list.get(0).getId());
+        personBatchStatusRecord.setBatchId(list.get(0).getBatchId());
+        personBatchStatusRecord.setPersonIdNumber(list.get(0).getIdNumber());
+        personBatchStatusRecord.setStatusStr(securityUser.getLoginName() + "-更新虚假材料信息");
+        personBatchStatusRecord.setStatusTime(new Date());
+        personBatchStatusRecord.setStatusReason("更新虚假材料信息");
+        personBatchStatusRecord.setStatusTypeDesc("更新虚假材料信息");
+        personBatchStatusRecord.setStatusInt(103);
+        iPersonBatchStatusRecordService.save(personBatchStatusRecord);
+
         return ResponseUtil.success();
     }
 
