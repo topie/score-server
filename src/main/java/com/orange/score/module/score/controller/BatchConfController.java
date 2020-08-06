50901da1e3c86d4c0617d2a09ecf420ebf2eb179
xueguangrui <10504228262qq.com>
2020-08-06 17:17:30 +0800

    统计

--- src/main/java/com/orange/score/module/score/controller/BatchConfController.java
+++ src/main/java/com/orange/score/module/score/controller/BatchConfController.java
@@ -1,5 +1,7 @@
 package com.orange.score.module.score.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageInfo;
 import com.orange.score.common.core.BaseController;
 import com.orange.score.common.core.Result;
@@ -10,15 +12,22 @@ import com.orange.score.common.utils.PageConvertUtil;
 import com.orange.score.common.utils.ResponseUtil;
 import com.orange.score.database.score.model.AcceptDateConf;
 import com.orange.score.database.score.model.BatchConf;
+import com.orange.score.database.score.model.IdentityInfo;
 import com.orange.score.module.core.service.ICommonQueryService;
 import com.orange.score.module.core.service.IDictService;
 import com.orange.score.module.score.service.IAcceptDateConfService;
 import com.orange.score.module.score.service.IBatchConfService;
+import com.orange.score.module.score.service.IIdentityInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+import tk.mybatis.mapper.entity.Condition;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -39,6 +48,9 @@ public class BatchConfController extends BaseController {
 
     @Autowired
     private IAcceptDateConfService iAcceptDateConfService;
+
+    @Autowired
+    private IIdentityInfoService iIdentityInfoService;
 
     @GetMapping(value = "/list")
     @ResponseBody
@@ -128,5 +140,58 @@ public class BatchConfController extends BaseController {
         Map result = new HashMap();
         result.put("html", html);
         return ResponseUtil.success(result);
+    }
+
+    /**
+     * 2020年8月6日
+     * 统计数据给统计系统
+     * @return
+     */
+    @RequestMapping("/provideDataToZhangLaoshi")
+    public Result provideDataToZhangLaoshi(HttpServletRequest httpServletRequest,
+                                           HttpServletResponse httpServletResponse){
+        String strDate = "2018-07";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        Date date201807;
+        Result result = new Result();
+
+
+
+        try {
+            date201807 = sdf.parse(strDate);
+            Condition condition = new Condition(BatchConf.class);
+            tk.mybatis.mapper.entity.Example.Criteria criteria = condition.createCriteria();
+            criteria.andGreaterThan("applyBegin",date201807);
+            condition.orderBy("id").asc();
+            List<BatchConf> list = iBatchConfService.findByCondition(condition);
+
+            JSONArray jsonArray = new JSONArray();
+
+            for(BatchConf batchConf : list){
+                condition = new Condition(IdentityInfo.class);
+                criteria = condition.createCriteria();
+                criteria.andEqualTo("batchId", batchConf.getId());
+                List<IdentityInfo> identityInfoList = iIdentityInfoService.findByCondition(condition);
+
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.put("id",batchConf.getId());
+                jsonObject.put("batchName",batchConf.getBatchName());
+                jsonObject.put("indicatorValue",batchConf.getIndicatorValue());
+                jsonObject.put("acceptUserCount",identityInfoList.size());
+                jsonObject.put("status",batchConf.getStatus());
+                jsonObject.put("scoreValue",batchConf.getScoreValue());
+                jsonArray.add(jsonObject);
+            }
+
+            result.setCode(1000);
+            result.setMessage("成功");
+            result.setData(jsonArray);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return  result;
     }
 }
